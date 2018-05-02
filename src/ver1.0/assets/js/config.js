3ed8da90f3914264ceb4e48da70747a0b789a515
zhaoming <zhaoming@mallcoo.cn>
2018-05-02 17:49:20 +0800

    style improve

--- src/ver1.0/assets/js/config.js
+++ src/ver1.0/assets/js/config.js
@@ -4,7 +4,5 @@ var _config = {
     repo            : 'snaildev.github.io',// github 中对应项目名
     // duoshuo_id      : 'hello1234',            // 在第三方评论插件多说申请站点的子域名
     // access_token : '',                     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
-    per_page        : '15'                    // 默认一页显示几篇文章
+    per_page        : '10'                    // 默认一页显示几篇文章
 }
-
-// var duoshuoQuery = {short_name:_config['duoshuo_id']};
